e8ff738029e56a12709e64fac4a766b63af5b244
addison <factor@DESKTOP-DFEJACF.localdomain>
2021-04-26 00:18:23 -0500

    Finished connecting to api

--- LofiWeather/app.js
+++ LofiWeather/app.js
@@ -2,25 +2,40 @@
 window.addEventListener('load', ()=> {
     let lon;
     let lat;
+    let tempDescription = document.querySelector('.temp-description');
+    let tempDegree = document.querySelector('.temp-degree');
+    let localTimeZone = document.querySelector('.location-timezone');
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
+            console.log(position)
+
             lon = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={bd62628f129a817f1d50db7210b477b0}`;
+            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=bd62628f129a817f1d50db7210b477b0`;
 
             fetch(api)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
+                .then(response => {
+                return response.json(); //parse the data
+                })
+                .then(data => {
                 console.log(data);
-            });
+                const {description, icon}= data.weather[0];
+                const {temp} = data.main;
+                const {name} = data;
+                //set Dom Elements from API
+                tempDescription.textContent = description;
+                tempDegree.textContent = temp;
+                localTimeZone.textContent = name;
+                //set icon
+                    
+                })
         });
     }
-        
 });
+
+
 
 //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 //2caefa76186fe46255b40dc7b1ba774c
