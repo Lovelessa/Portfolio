059e2b4a1224015bd1ad9ebfc10a7ad27549b9fd
addison <factor@DESKTOP-DFEJACF.localdomain>
2021-04-27 01:23:26 -0500

    incorporated the icon function, cleaned up code and comments

--- LofiWeather/app.js
+++ LofiWeather/app.js
@@ -1,15 +1,19 @@
 // after page loads function runs to grab location
 window.addEventListener('load', ()=> {
+    //selected elements
     let lon;
     let lat;
     let tempDescription = document.querySelector('.temp-description');
     let tempDegree = document.querySelector('.temp-degree');
     let localTimeZone = document.querySelector('.location-timezone');
-
+    let locationIcon = document.querySelector('.weather-icon');
+    
+    //grabbing the geolocation from the browser 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             console.log(position)
-
+            
+            //passing the coords from the browser into variables
             lon = position.coords.longitude;
             lat = position.coords.latitude;
 
@@ -21,22 +25,22 @@ window.addEventListener('load', ()=> {
                 })
                 .then(data => {
                 console.log(data);
-                const {description, icon}= data.weather[0];
+                const {description} = data.weather[0];
+                const {icon} = data.weather[0];
                 const {temp} = data.main;
                 const {name} = data;
                 //set Dom Elements from API
                 tempDescription.textContent = description;
                 tempDegree.textContent = temp;
                 localTimeZone.textContent = name;
-                //set icon
+                //link icon
+                locationIcon.innerHTML = `<img src="icons/${icon}.png"/>`;
                     
                 })
         });
     }
+
 });
 
 
 
-//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-//2caefa76186fe46255b40dc7b1ba774c
-//bd62628f129a817f1d50db7210b477b0
