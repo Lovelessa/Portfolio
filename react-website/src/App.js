66f878ffcd27d488a12232fafe8140a30ae67884
addison <factor@DESKTOP-DFEJACF.localdomain>
2021-05-09 20:13:17 -0500

    first commit on react-project

---
+++ react-website/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './components/navbar';
+
+
+function App() {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo>
+            Logo
+          </NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+}
+
+export default App;
