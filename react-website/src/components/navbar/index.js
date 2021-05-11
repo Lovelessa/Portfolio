acc030aeb0ba3f350c792fa63219f50bbe090a53
addison <factor@DESKTOP-DFEJACF.localdomain>
2021-05-11 17:06:18 -0500

    Finished sidebar and hero section

--- react-website/src/components/navbar/index.js
+++ react-website/src/components/navbar/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {FaBars} from 'react-icons/fa'
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './navbarElements'
 
-const Navbar = () => {
+const Navbar = ({toggle}) => {
     return (
         <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to='/'>Logo</NavLogo>
-          <MobileIcon>
+          <NavLogo to='/'>PrysmWare</NavLogo>
+          <MobileIcon onClick={toggle}>
               <FaBars />
           </MobileIcon>
           <NavMenu>
